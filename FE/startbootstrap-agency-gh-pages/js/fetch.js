76e81697ab35e13624b41c8e4bf7c5bc449bb5a0
Ruben Faria <rubenfar5@gmail.com>
2021-03-15 11:01:24 +0000

    Create fetch.js

---
+++ FE/startbootstrap-agency-gh-pages/js/fetch.js
@@ -0,0 +1,37 @@
+//---------------------funcao para fazer Post de suspeitos-----------------------------------------
+
+function saveSuspect() {
+    var data = {};
+    data.username = document.getElementById("exampleFirstName").value;
+    data.email = document.getElementById("exampleInputEmail").value;
+    data.password = document.getElementById("exampleInputPassword").value;
+    data.tipo = "familia";
+  
+    console.log(data); //debugging para ver os dados que foram enviados
+  
+    //chamada fetch para envio dos dados para o servior via POST
+    fetch( 'localhost:8080/users',
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    )
+      .then(function (response) {
+        if (!response.ok) {
+          console.log(response.status); //=> number 100–599
+          console.log(response.statusText); //=> String
+          console.log(response.headers); //=> Headers
+        } else {
+          console.log("Success POST");
+          console.log(response);
+        }
+      })
+      .then(function (result) {
+        console.log(result);
+      })
+      .catch(function (err) {
+        alert("Submission error");
+        console.error(err);
+      });
+  }
