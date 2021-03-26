0655ad45bcb94788ff0c88d19c7ae3d2c4143a98
NunoAmorim22 <73607119+NunoAmorim22@users.noreply.github.com>
2021-03-26 11:26:43 +0000

    (.)(.)

--- FE/startbootstrap-agency-gh-pages/js/team.js
+++ FE/startbootstrap-agency-gh-pages/js/team.js
@@ -1,5 +1,21 @@
 var slideIndex = 1;
-var fotos1 = [["assets/img/team/Nuno.png", "Aqui vê-se um nabo!"], ["assets/img/team/Ruben.png", "Idem"], ["assets/img/team/Eu.png", "E por ultimo, mas muito mais importante.<p>Ele!</p> <p>O grande!</p> <p>O génio!</p> <p>Vasco Pereira!!!</p>", "assets/audios/ooh.wav"]];
+var fotos1 = 
+[
+["assets/img/team/Nuno.png",""],
+["assets/img/team/AnaCatarina.jpeg",""],
+["assets/img/team/carol.jpg",""],
+["assets/img/team/catarinasilva.jpg",""],
+["assets/img/team/MarianaCancela.jpg",""],
+["assets/img/team/",""],
+["assets/img/team/",""],
+["assets/img/team/", ""],
+["assets/img/team/", ""],
+["assets/img/team/", ""],
+["assets/img/team/", ""],
+["assets/img/team/", ""],
+["assets/img/team/", ""],
+["assets/img/team/", ""],
+];
 var slides = fotos1.length;
 showSlides(slideIndex);
 console.log(fotos1.length);
@@ -23,22 +39,22 @@ function showSlides(n) {
     if (n > slides) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides }
     for (i = 0; i < fotos1.length; i++) {
-        if(slideIndex == i + 1){
+        if (slideIndex == i + 1) {
             dots = dots + `<span class="dot active" onclick="currentSlide(${i + 1})"></span>`;
         }
-        else{
-        dots = dots + `<span class="dot" onclick="currentSlide(${i + 1})"></span>`;
+        else {
+            dots = dots + `<span class="dot" onclick="currentSlide(${i + 1})"></span>`;
         }
-    }   
+    }
     document.getElementById("rights-texts").innerHTML = dots;
-    
+
     //profun
-    if(slideIndex == 3){
+    if (slideIndex == 3) {
         document.getElementById("teamSlides").innerHTML = `<div class="numbertext">${slideIndex} / ${fotos1.length}</div><br> <br><div><img  class="responsive rounded-circle" src='${fotos1[slideIndex - 1][0]}' style="display: block;  margin-left: auto; margin-right: auto;"/></div> <br> <br> <div class="text responsive">${fotos1[slideIndex - 1][1]}</p><audio controls src="${fotos1[slideIndex - 1][2]}">AQUI</audio></div> `;
 
     }
-    else{
-    document.getElementById("teamSlides").innerHTML = `<div class="numbertext">${slideIndex} / ${fotos1.length}</div><br> <br><div><img  class="responsive rounded-circle" src='${fotos1[slideIndex - 1][0]}' style="display: block;  margin-left: auto; margin-right: auto;"/></div> <br> <br> <div class="text responsive">${fotos1[slideIndex - 1][1]}</p>`;
+    else {
+        document.getElementById("teamSlides").innerHTML = `<div class="numbertext">${slideIndex} / ${fotos1.length}</div><br> <br><div><img  class="responsive rounded-circle" src='${fotos1[slideIndex - 1][0]}' style="display: block;  margin-left: auto; margin-right: auto;"/></div> <br> <br> <div class="text responsive">${fotos1[slideIndex - 1][1]}</p>`;
     }
 
 
