b48926185ff3f5ea7cfc2c8a84981c480e544775
dagama14 <73606475+dagama14@users.noreply.github.com>
2021-03-29 09:49:21 +0100

    Incrivel

--- FE/startbootstrap-agency-gh-pages/js/livro.js
+++ FE/startbootstrap-agency-gh-pages/js/livro.js
@@ -70,6 +70,9 @@ function showSlides(n) {
         else{
         dots = dots + `<span class="dot" onclick="currentSlide(${i + 1})">${i + 1}</span>`;
         }
+        if(slideIndex == i - 5){
+            break;
+        }
     }   
     document.getElementById("rights-texts").innerHTML = dots;
     document.getElementById("mySlides").innerHTML = `<div class="numbertext">${slideIndex} / ${fotos1.length}</div><br> <br><div><img  class="responsive" src='${fotos1[slideIndex - 1][0]}' style="display: block; width:50%; margin-left: auto; margin-right: auto;"/></div> <br> <br> <div class="text responsive">${fotos1[slideIndex - 1][1]}</p><audio controls src="${fotos1[slideIndex - 1][2]}">AQUI</audio></div> <br> `;
