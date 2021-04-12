e89b9113c65cf7a39554a6fecbe2baf55a4a6ebb
NunoAmorim22 <73607119+NunoAmorim22@users.noreply.github.com>
2021-04-12 09:48:17 +0100

    seta paneleira para voltar para o topo da pagina

--- FE/startbootstrap-agency-gh-pages/js/scripts.js
+++ FE/startbootstrap-agency-gh-pages/js/scripts.js
@@ -151,3 +151,24 @@ function activeEdit(){
 function showAddForm(){
     document.getElementById("formAdd").style.display = "";
 }
+
+//---------------------------------------Botão para ir para o topo da página------------------------------------------------------------------------
+//Get the button
+var mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
