2d248f870eedcb05945343fb0443143b1cefc170
dagama14 <73606475+dagama14@users.noreply.github.com>
2021-04-14 00:53:18 +0100

    Pussy

--- FE/startbootstrap-agency-gh-pages/js/forum.js
+++ FE/startbootstrap-agency-gh-pages/js/forum.js
@@ -1,70 +1,102 @@
 let forum;
-
+let messages;
 function addDiscuss() {
     let data = {};
     data.nome = document.getElementById("novaDiscussaoTitulo").value;
     data.descricao = document.getElementById("novaDiscussaoTexto").value;
-    data.userId = {
-        "id": 1,
-        "username": "gil",
-        "email": "berto@gmail.com",
-        "password": "$2a$10$8Y94FGrtjXAjpGqUT2RrtOObb1Io0nwoWX2BnnwjTCJasr03cI8lm",
-        "tipo": "instituicao",
-        "enabled": true,
-        "authorities": [
-            {
-                "authority": "instituicao"
-            }
-        ],
-        "accountNonLocked": true,
-        "accountNonExpired": true,
-        "credentialsNonExpired": true
-    };
-   console.log(data);
+    data.usersId = "1";
+    console.log(data);
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-  
-  
+
+
     var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: JSON.stringify(data),
-      redirect: 'follow'
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(data),
+        redirect: 'follow'
     };
-  
+
     fetch("http://localhost:8080/prochild/topicos", requestOptions)
-      .then(function (response) {
-        if (!response.ok) {
-          console.log(response.status); //=> number 100–599
-          console.log(response.statusText); //=> String
-          console.log(response.headers); //=> Headers
-        } else {
-          console.log("Success POST");
-          console.log(response);
-          window.location.href = "./MenuJogos.html";
-        }
-      })
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-  
-  
-  }
+        .then(function (response) {
+            if (!response.ok) {
+                console.log(response.status); //=> number 100–599
+                console.log(response.statusText); //=> String
+                console.log(response.headers); //=> Headers
+            } else {
+                console.log("Success POST");
+                console.log(response);
+                window.location.href = "./MenuForum.html";
+            }
+        })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+
+}
+function answer(topico) {
+    let data = {};
+    data.conteudo = document.getElementById("novaMensagem").value;
+    data.usersId = "1";
+    data.topicosId = topico;
+    console.log(data);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(data),
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:8080/prochild/mensagens", requestOptions)
+        .then(function (response) {
+            if (!response.ok) {
+                console.log(response.status); //=> number 100–599
+                console.log(response.statusText); //=> String
+                console.log(response.headers); //=> Headers
+            } else {
+                console.log("Success POST");
+                console.log(response);
+                window.location.href = "./MenuForum.html";
+
+            }
+        })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+
+}
 function fetchForums() {
     async function fetchAsync() {
-      const response = await fetch(`http://localhost:8080/prochild/topicos`);
-       forum = await response.json()
-      console.log(forum);
-      show();
-      
+        const response = await fetch(`http://localhost:8080/prochild/topicos`);
+        forum = await response.json()
+        console.log(forum);
+        show();
+
     }
     fetchAsync()
-      .then((data) => console.log("ok"))
-      .catch((reason) => console.log(reason.message));
-  }
-  
-  
-  function show() {
+        .then((data) => console.log("ok"))
+        .catch((reason) => console.log(reason.message));
+}
+function fetchMessages() {
+    async function fetchAsync() {
+        const response = await fetch(`http://localhost:8080/prochild/mensagens`);
+        messages = await response.json()
+        console.log(messages);
+        reloadSame();
+
+    }
+    fetchAsync()
+        .then((data) => console.log("ok"))
+        .catch((reason) => console.log(reason.message));
+}
+
+function show() {
     let tab = ` <thead>
     <th>
         Nome
@@ -76,19 +108,20 @@ function fetchForums() {
 
     </th>
 </thead>`;
-  
+
     // Loop to access all rows
-    for (let r of forum) {
-      tab += `<tbody>
+    for (let r = 0; r < forum.length; r++) {
+        tab += `<tbody>
       <tr>
           <td>
               ${forum[r].nome}
           </td>
           <td>
-              ${forum[r].autor}
+              ${forum[r].usersId.username}
           </td>
           <td>
-              <a onclick="showDetail(${forum[r].id})"><i class="fas fa-search-plus"></i> </a>
+              <a onclick="showDetail(${forum[r].id}, '${forum[r].nome}'); "><i class="fas fa-search-plus"></i> </a>
+              <a name="garbage" id="${forum[r].usersId.id}" style="display: none;" onclick="deleteMessage(${forum[r].id});"><i class="far fa-trash-alt"></i> </a>
           </td>
 
       </tr>
@@ -96,5 +129,97 @@ function fetchForums() {
     }
     // Setting innerHTML as tab variable
     document.getElementById("discussTable").innerHTML = tab;
-  }
-  
+    garbage();
+}
+fetchMessages();
+function showDetail(id, title) {
+    localStorage.setItem("titleMessage", title);
+    localStorage.setItem("idMessage", id);
+    document.getElementById("titulo").innerHTML = title;
+    document.getElementById("sexaoMensagem").style.display = "";
+    let resp = `<thead>
+    <th>
+        Mensagem
+    </th>
+    <th>
+        Autor
+    </th>
+</thead>
+<tbody>`;
+    console.log(messages);
+    for (let i = 0; i < messages.length; i++) {
+        if (messages[i].topicosId.id == id) {
+            resp += `
+            <tr>
+                <td>
+                ${messages[i].conteudo}
+                </td>
+                <td>
+                ${messages[i].usersId.username}
+                </td>
+            </tr>
+       `;
+        }
+    }
+    resp += `</tbody>`;
+    document.getElementById("messageTable").innerHTML = resp;
+    console.log(id);
+    document.getElementById("btnSendAnswer").setAttribute("onclick", `answer(${id})`);
+}
+
+function reloadSame() {
+    if (localStorage.hasOwnProperty("titleMessage") && localStorage.hasOwnProperty("idMessage")) {
+        showDetail(localStorage.getItem("idMessage"), localStorage.getItem("titleMessage"));
+    }
+}
+
+
+function deleteMessage(id) {
+    var requestOptions = {
+        method: 'DELETE',
+    };
+    console.log(id);
+    //selecionar o id do jogo selecionado
+    for(let i=0;i<messages.length;i++){
+        let message = messages[i].topicosId.id;
+        if(message == id){
+            fetch(`http://localhost:8080/prochild/mensagens/${messages[i].id}`, requestOptions)
+            .then(function (response) {
+                if (!response.ok) {
+                    console.log(response.status); //=> number 100–599
+                    console.log(response.statusText); //=> String
+                    console.log(response.headers); //=> Headers
+                } else {
+                    console.log("Success POST");
+                    console.log(response);
+                }
+            })
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error)); 
+        }
+    }
+    deleteForum(id);
+}
+function deleteForum(params) {
+    var requestOptions = {
+        method: 'DELETE',
+    };
+
+    fetch(`http://localhost:8080/prochild/topicos/${params}`, requestOptions)
+        .then(function (response) {
+            if (!response.ok) {
+                console.log(response.status); //=> number 100–599
+                console.log(response.statusText); //=> String
+                console.log(response.headers); //=> Headers
+            } else {
+                console.log("Success POST");
+                console.log(response);
+                window.location.href = "./MenuForum.html";
+            }
+        })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+}
--- FE/startbootstrap-agency-gh-pages/js/scripts.js
+++ FE/startbootstrap-agency-gh-pages/js/scripts.js
@@ -3,14 +3,14 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
     */
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
+            this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
@@ -58,64 +58,64 @@
 
 //-------------------------------------------NOVO----------------------------------------------------//
 
-function chooseTypeChild(){
+function chooseTypeChild() {
     localStorage.setItem("type", "child");
     window.location.href = "./MenuEscolherIdade.html";
 }
 
-function chooseTypeFamily(){
+function chooseTypeFamily() {
     localStorage.setItem("type", "family");
     window.location.href = "./LoginFamilias.html";
 }
 
-function chooseTypeInstitution(){
+function chooseTypeInstitution() {
     localStorage.setItem("type", "institution");
     window.location.href = "./LoginInstituicoes.html";
 }
 
-function moveOn(){
-    if(localStorage.getItem("age")!== null && localStorage.getItem("gender") !== null){
+function moveOn() {
+    if (localStorage.getItem("age") !== null && localStorage.getItem("gender") !== null) {
         window.location.href = "./MenuPrincipal.html";
     }
 }
 
-function chooseGenderMale(){
+function chooseGenderMale() {
     localStorage.setItem("gender", "male");
     document.getElementById("male-btn").style.backgroundColor = "rgba(249, 210, 77, 1)";
     document.getElementById("female-btn").style.backgroundColor = "rgba(255, 255, 255, 0)";
 }
-function chooseGenderFemale(){
+function chooseGenderFemale() {
     localStorage.setItem("gender", "female");
     document.getElementById("male-btn").style.backgroundColor = "rgba(255, 255, 255, 0)";
     document.getElementById("female-btn").style.backgroundColor = "rgba(249, 210, 77, 1)";
 }
 
-function chooseAge(faixa){
+function chooseAge(faixa) {
     localStorage.setItem("age", faixa);
-    for(i=1;i<5;i++){
-        if(faixa == i){
+    for (i = 1; i < 5; i++) {
+        if (faixa == i) {
             document.getElementById(i + "btn").style.backgroundColor = "rgba(249, 210, 77, 1)";
         }
-        else{
+        else {
             document.getElementById(i + "btn").style.backgroundColor = "rgba(255, 255, 255, 0)";
         }
     }
 }
 
-function clearLocalStorage(){
+function clearLocalStorage() {
     localStorage.clear();
 }
 
-function changeMainMenu(){
-    if(localStorage.getItem("type") == "child"){
-        if(localStorage.getItem("age") === "1" || localStorage.getItem("age") === "2" || localStorage.getItem("age") === "3"){
+function changeMainMenu() {
+    if (localStorage.getItem("type") == "child") {
+        if (localStorage.getItem("age") === "1" || localStorage.getItem("age") === "2" || localStorage.getItem("age") === "3") {
             document.getElementById("forumbtn").remove();
             document.getElementById("direitosbtn").setAttribute("href", "MenuDireitosCriancas.html");
         }
         document.getElementById("perfilbtn").remove();
         document.getElementById("apoiosplace").innerHTML = '<a class="nav-link js-scroll-trigger" href="MenuSelecaoTipoInformacao.html"><i class="far fa-question-circle"></i> Ajudas</a>';
     }
-    else if(localStorage.getItem("function") == "security"){
+    else if (localStorage.getItem("function") == "security") {
         document.getElementById("apoiosplace").innerHTML = '<a class="nav-link js-scroll-trigger" href="MenuVerDenuncias.html"><i class="fas fa-shield-alt"></i> Denuncias</a>';
     }
     else {
@@ -123,8 +123,8 @@ function changeMainMenu(){
     }
 }
 
-function changeProfileMenu(){
-    if(localStorage.getItem("type") == "family"){
+function changeProfileMenu() {
+    if (localStorage.getItem("type") == "family") {
         document.getElementById("instituitiontype").remove();
     }
 }
@@ -136,37 +136,37 @@ function changeProfileMenu(){
 }*/
 
 /*-----------------------------------------------------------------------------------------------*/
-function editAssets(){
+function editAssets() {
     let btns = document.getElementsByName("editingbtn");
-    for(n = 0; n < btns.length; n++){
+    for (n = 0; n < btns.length; n++) {
         btns[n].style.display = "";
     }
 }
 
-function activeEdit(){
-    if(localStorage.getItem("type") == "institution" || localStorage.getItem("type") == "admin"){
+function activeEdit() {
+    if (localStorage.getItem("type") == "institution" || localStorage.getItem("type") == "admin") {
         document.getElementById("btnEdit").style.display = "";
     }
 }
 
-function showAddForm(){
+function showAddForm() {
     document.getElementById("formAdd").style.display = "";
     window.location.href = "#formAdd";
 }
 
 function hideOption() {
-    if(localStorage.getItem("type") != "child"){
+    if (localStorage.getItem("type") != "child") {
         document.getElementById("paraFamilias").style.display = "";
     }
-    
+
 }
 
 function perfil() {
-    if(localStorage.getItem("type") == "family"){
-      fetchFamilia();
-      document.getElementById("editBTN").setAttribute("onclick", "editDataFamily()");
+    if (localStorage.getItem("type") == "family") {
+        fetchFamilia();
+        document.getElementById("editBTN").setAttribute("onclick", "editDataFamily()");
     }
-    if(localStorage.getItem("type") == "institution"){
+    if (localStorage.getItem("type") == "institution") {
         fetchInstituicao();
         document.getElementById("editBTN").setAttribute("onclick", "editDataInstitution()");
     }
@@ -175,27 +175,39 @@ function perfil() {
 
 function editPass() {
     let btns = document.getElementsByName("pass");
-    for(n = 0; n < btns.length; n++){
+    for (n = 0; n < btns.length; n++) {
         btns[n].style.display = "";
     }
 }
+
+function garbage() {
+    let btns = document.getElementsByName("garbage");
+    console.log("ola");
+    for (n = 0; n < btns.length; n++) {
+        if (localStorage.getItem("type") === "Admin" || localStorage.getItem("LoggedIn") == btns[n].getAttribute("id")) {
+            btns[n].style.display = "";
+            console.log(btns[n].getAttribute("id"));
+        }
+    }
+}
+
 //---------------------------------------Botão para ir para o topo da página------------------------------------------------------------------------
 //Get the button
 var mybutton = document.getElementById("topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
