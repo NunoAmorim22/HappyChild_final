dd9dd90d3996dbeccf2abde695f01d256eb25fde
dagama14 <73606475+dagama14@users.noreply.github.com>
2021-04-13 21:08:08 +0100

    .|.

--- FE/startbootstrap-agency-gh-pages/js/forum.js
+++ FE/startbootstrap-agency-gh-pages/js/forum.js
@@ -1,9 +1,60 @@
 let forum;
+
+function addDiscuss() {
+    let data = {};
+    data.nome = document.getElementById("novaDiscussaoTitulo").value;
+    data.descricao = document.getElementById("novaDiscussaoTexto").value;
+    data.userId = {
+        "id": 1,
+        "username": "gil",
+        "email": "berto@gmail.com",
+        "password": "$2a$10$8Y94FGrtjXAjpGqUT2RrtOObb1Io0nwoWX2BnnwjTCJasr03cI8lm",
+        "tipo": "instituicao",
+        "enabled": true,
+        "authorities": [
+            {
+                "authority": "instituicao"
+            }
+        ],
+        "accountNonLocked": true,
+        "accountNonExpired": true,
+        "credentialsNonExpired": true
+    };
+   console.log(data);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+  
+  
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(data),
+      redirect: 'follow'
+    };
+  
+    fetch("http://localhost:8080/prochild/topicos", requestOptions)
+      .then(function (response) {
+        if (!response.ok) {
+          console.log(response.status); //=> number 100–599
+          console.log(response.statusText); //=> String
+          console.log(response.headers); //=> Headers
+        } else {
+          console.log("Success POST");
+          console.log(response);
+          window.location.href = "./MenuJogos.html";
+        }
+      })
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  
+  
+  }
 function fetchForums() {
     async function fetchAsync() {
       const response = await fetch(`http://localhost:8080/prochild/topicos`);
        forum = await response.json()
-      console.log(livros);
+      console.log(forum);
       show();
       
     }
@@ -12,7 +63,6 @@ function fetchForums() {
       .catch((reason) => console.log(reason.message));
   }
   
-  fetchForums();
   
   function show() {
     let tab = ` <thead>
